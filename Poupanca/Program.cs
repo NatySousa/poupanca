af6639320c508a7ed88b4129117d27eb999c7574
NatySousa <nataliaspsousa@gmail.com>
2022-04-28 15:49:35 -0300

    Projeto Console Aplication

---
+++ Poupanca/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Poupanca
+{
+    
+    public class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Calculando rendimentos da Poupança");
+
+            double valorInvestido = 1000;
+            int contadorMes = 1;
+
+            //rendimento mensal 0,36% = 0.0036
+
+            while (contadorMes <= 12)
+            {
+                valorInvestido = valorInvestido + valorInvestido * 0.0036;
+
+                Console.WriteLine("Após " + contadorMes + "meses, você terá R$" + valorInvestido + " reais.");
+
+                contadorMes++;
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
